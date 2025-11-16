d9aa850d02ea9a245563b06f7223d447f6ee0e5c
Renaud Manet <renaud.manet@epitech.eu>
2025-11-16 03:32:32 +0100

    bootstrap: add Part2-SExpr-Ex2

--- lisp/Main.hs
+++ lisp/Main.hs
@@ -8,6 +8,17 @@ data SExpr
   | SList [SExpr]
   deriving Show
 
+getInteger :: SExpr -> Maybe Integer
+getInteger (SInteger n) = Just n
+getInteger _ = Nothing
+
+getSymbol :: SExpr -> Maybe String
+getSymbol (SSymbol s) = Just s
+getSymbol _ = Nothing
+
+getList :: SExpr -> Maybe [SExpr]
+getList (SList xs) = Just xs
+getList _ = Nothing
 main :: IO ()
 main =
   exitSuccess
