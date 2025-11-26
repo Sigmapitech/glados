fd377013c19ebff8c52bffda6c43c1346d591bc9
Renaud Manet <renaud.manet@epitech.eu>
2025-11-26 17:37:15 +0100

    lisp: define an AST of basic lisp language features

--- lisp/src/Ast.hs
+++ lisp/src/Ast.hs
@@ -8,7 +8,13 @@ module Ast where
 
 import Data.String (IsString (..))
 
+-- | Variable name in the program
+newtype VarName = VarName String
+  deriving (Show, Eq, Ord, IsString)
 
+-- | Parameter name in lambda/function definition
+newtype ParamName = ParamName String
+  deriving (Show, Eq, Ord, IsString)
 
 -- | Symbol from parser (before semantic analysis)
 newtype SymbolName = SymbolName String
@@ -18,7 +24,13 @@ newtype SymbolName = SymbolName String
 newtype ErrorMsg = ErrorMsg String
   deriving (Show, Eq, IsString)
 
+-- | Extract the raw string from a VarName
+unVarName :: VarName -> String
+unVarName (VarName s) = s
 
+-- | Extract the raw string from a ParamName
+unParamName :: ParamName -> String
+unParamName (ParamName s) = s
 
 -- | Extract the raw string from a SymbolName
 unSymbolName :: SymbolName -> String
@@ -28,7 +40,13 @@ unSymbolName (SymbolName s) = s
 unErrorMsg :: ErrorMsg -> String
 unErrorMsg (ErrorMsg s) = s
 
+-- | Convert a parameter name to a variable name
+paramToVar :: ParamName -> VarName
+paramToVar (ParamName s) = VarName s
 
+-- | Convert a variable name to a parameter name
+varToParam :: VarName -> ParamName
+varToParam (VarName s) = ParamName s
 
 -- | Convert a symbol name to a variable name
 symbolToVar :: SymbolName -> VarName
@@ -46,6 +64,23 @@ data SExpr
     SList [SExpr]
   deriving (Show, Eq)
 
+-- | Abstract Syntax Tree representation after semantic analysis
+data Ast
+  = -- | Integer literal
+    LiteralInt Integer
+  | -- | Boolean literal
+    LiteralBool Bool
+  | -- | Variable reference
+    Variable VarName
+  | -- | Variable definition (Function or value)
+    Define VarName Ast
+  | -- | Anonymous function
+    Lambda [ParamName] Ast
+  | -- | Function call
+    Call Ast [Ast]
+  | -- | Conditional expression
+    If {ifCond :: Ast, ifThen :: Ast, ifElse :: Ast}
+  deriving (Show, Eq)
 
 -- | Generic result type (either error or success)
 type Result a = Either ErrorMsg a
@@ -53,6 +88,8 @@ type Result a = Either ErrorMsg a
 -- | Result type for parsing operations
 type ParseResult = Either ErrorMsg SExpr
 
+-- | Result type for AST conversion operations
+type ConvertResult = Either ErrorMsg Ast
 
 -- | Helper to create error messages
 mkError :: String -> ErrorMsg
@@ -63,6 +100,35 @@ mkError = ErrorMsg
 liftError :: String -> Either String a -> Either ErrorMsg a
 liftError context (Left err) = Left $ ErrorMsg $ context ++ ": " ++ err
 liftError _ (Right val) = Right val
+
+-- | Built-in arithmetic operators
+builtinPlus, builtinMinus, builtinMult :: VarName
+builtinPlus = VarName "+"
+builtinMinus = VarName "-"
+builtinMult = VarName "*"
+
+-- | Built-in division and modulo operators
+builtinDiv, builtinMod :: VarName
+builtinDiv = VarName "div"
+builtinMod = VarName "mod"
+
+-- | Built-in comparison operators
+builtinEq, builtinLt :: VarName
+builtinEq = VarName "eq?"
+builtinLt = VarName "<"
+
+-- | Check if a variable name refers to a built-in function
+isBuiltin :: VarName -> Bool
+isBuiltin name =
+  name
+    `elem` [ builtinPlus,
+             builtinMinus,
+             builtinMult,
+             builtinDiv,
+             builtinMod,
+             builtinEq,
+             builtinLt
+           ]
 
 -- | Special form keywords used by the parser
 defineSymbol, lambdaSymbol, ifSymbol :: SymbolName
