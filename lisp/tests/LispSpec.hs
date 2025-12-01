e1fa71ca661928806cfc15aa94d5096a3445f71c
Renaud Manet <renaud.manet@epitech.eu>
2025-12-01 13:27:45 +0100

    lisp: add unit tests for the AST module

--- lisp/tests/LispSpec.hs
+++ lisp/tests/LispSpec.hs
@@ -7,9 +7,11 @@
 
 module Main (main) where
 
+import LispAstSpec (astModuleSpec)
 import LispBootstrapSpec (bootstrapModuleSpec)
 import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
   bootstrapModuleSpec
+  astModuleSpec
