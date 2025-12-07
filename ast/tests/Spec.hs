9294a517c512cf967e2e47b4111a9d4ad5da3f77
Sigmanificient <edhyjox@gmail.com>
2025-12-07 06:07:42 +0100

    ast: extract module from lisp

--- ast/tests/Spec.hs
+++ ast/tests/Spec.hs
@@ -1,13 +1,10 @@
-module Main (main) where
+module Main where
 
-import Test.Hspec
-
-dummySpec :: Spec
-dummySpec = do
-  describe "dummy test" $ do
-    it "Should always pass" $ do
-      True `shouldBe` True
+import SpecAST (specAST)
+import SpecSexprtoAST (spec)
+import Test.Hspec (hspec)
 
 main :: IO ()
 main = hspec $ do
-  dummySpec
+  specAST
+  spec
