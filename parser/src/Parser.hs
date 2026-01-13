9c635b1566ce68046c5178e3eafd2c04a3a1f940
Fenriir42 <raid3r2205@gmail.com>
2026-01-13 17:25:27 +0100

    parser: enhance literal parsing with array and float support

--- parser/src/Parser.hs
+++ parser/src/Parser.hs
@@ -1,18 +1,40 @@
 module Parser where
 
-import AST.Types.AST
-import AST.Types.Common (Located (..), SourceSpan (..))
+import AST.Types.Common (Located (..))
 import AST.Types.Literal
+  ( ArrayLiteral (..),
+    BoolLiteral (..),
+    FloatLiteral (..),
+    IntBase (..),
+    IntLiteral (..),
+    Literal (..),
+    StringLiteral (..),
+  )
 import AST.Types.Operator
+  ( AssignOp,
+    BinaryOp,
+    UnaryOp,
+    symbolToAssignOp,
+    symbolToBinaryOp,
+    symbolToUnaryOp,
+  )
 import qualified Data.Set as Set
 import Data.Text (Text)
 import Error (ParseError (..))
-import Text.Megaparsec (ParseErrorBundle)
 import qualified Text.Megaparsec as MP
 import Tokens (Token, TokenConent (..))
 import Prelude hiding (span)
 
 type TokenParser = MP.Parsec ParseError [Token]
+
+-- Helper to match a specific symbol
+matchSymbol :: Text -> TokenParser (Located Text)
+matchSymbol sym = do
+  Located span (TokSymbol s) <- MP.satisfy isMatchingSymbol
+  return $ Located span s
+  where
+    isMatchingSymbol (Located _ (TokSymbol s)) = s == sym
+    isMatchingSymbol _ = False
 
 parseOperator :: [Text] -> (Text -> Maybe a) -> TokenParser (Located a)
 parseOperator validOps converter = do
@@ -65,8 +87,29 @@ parseIntLiteral = do
 
 parseFloatLiteral :: TokenParser (Located FloatLiteral)
 parseFloatLiteral = do
-  Located span (TokFloat f) <- MP.satisfy isFloat
-  return $ Located span (FloatLiteral f)
+  Located intSpan (TokInt n BaseDec) <- MP.satisfy isInt
+  Located decimalSpan _ <- matchSymbol "."
+  Located fracSpan (TokInt fracN BaseDec) <- MP.satisfy isInt
+  let floatVal = read (show n ++ "." ++ show fracN) :: Double
+      combinedSpan = intSpan <> decimalSpan <> fracSpan
+  return $ Located combinedSpan (FloatLiteral floatVal)
   where
-    isFloat (Located _ (TokFloat _)) = True
-    isFloat _ = False
+    isInt (Located _ (TokInt _ _)) = True
+    isInt _ = False
+
+parseArrayLiteral :: TokenParser expr -> TokenParser (Located (ArrayLiteral expr))
+parseArrayLiteral parseElement = do
+  Located startSpan _ <- matchSymbol "["
+  elements <- MP.sepBy parseElement (matchSymbol ",")
+  Located endSpan _ <- matchSymbol "]"
+  return $ Located (startSpan <> endSpan) (ArrayLiteral elements)
+
+parseLiteral :: TokenParser expr -> TokenParser (Located (Literal expr))
+parseLiteral parseExpr =
+  MP.choice
+    [ fmap LitFloat <$> parseFloatLiteral,
+      fmap LitInt <$> parseIntLiteral,
+      fmap LitBool <$> parseBoolLiteral,
+      fmap LitString <$> parseStringLiteral,
+      fmap LitArray <$> parseArrayLiteral parseExpr
+    ]
